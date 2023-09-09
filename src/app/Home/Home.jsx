05ff1d5fa931dc07e61c5ffc9ef2ca61c26dbfe7
Jiozang theophane <jiozangtheophane@gmail.com>
2023-09-09 16:51:25 +0200

    jj

--- src/app/Home/Home.jsx
+++ src/app/Home/Home.jsx
@@ -1,16 +1,18 @@
-import Banner from '../components/Banner/Banner';
-import Features from '../components/Features/Features';
-import Footer from '../components/Footer/Footer';
-import './Home.css';
+import Banner from "../components/Banner/Banner";
+import Features from "../components/Features/Features";
+import Footer from "../components/Footer/Footer";
+import "./Home.css";
 
 function Home() {
-    return <div>
-        <Banner />
-        <main>
+  return (
+    <div>
+      <Banner />
+      <main>
         <Features />
-        </main>
-        <Footer /> 
+      </main>
+      <Footer />
     </div>
+  );
 }
 
-export default Home
+export default Home;
