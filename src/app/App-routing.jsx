239e2d8f20929eda12a0a9f113e124bd3c7db570
theoleprince <jiozangtheophane@gmail.com>
2023-09-22 07:33:40 +0100

    mer

--- src/app/App-routing.jsx
+++ src/app/App-routing.jsx
@@ -2,11 +2,12 @@ import { Routes, Route } from 'react-router-dom'
 import Home from './Home/Home'
 import User from './User/User'
 import SingIn from './Sign-in/SignIn'
+import withAuth from './_services/auth-guard'
 
 function AppRouting() {
     return <Routes>
     <Route path="/" element={<Home />} />
-    <Route path="/user" element={<User />} />
+    <Route path="/user" element={withAuth(<User />)} />
     <Route path="/sign-in" element={<SingIn />} />
     <Route path="*" element={<Home />} />
   </Routes>
